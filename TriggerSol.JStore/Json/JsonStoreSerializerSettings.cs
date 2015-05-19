33d76d6ee038d2af80d2902b853ce8f92bb0cd00
Jörg Egger <joerg.egger@outlook.de>
2015-05-19 16:13:20 +0200

    Code formatting

--- TriggerSol.JStore/Json/JsonStoreSerializerSettings.cs
+++ TriggerSol.JStore/Json/JsonStoreSerializerSettings.cs
@@ -24,7 +24,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using TriggerSol.Dependency;
